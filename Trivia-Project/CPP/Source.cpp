48cfa8630b834f1aafac9f7d50f06c3fe026a013
Elior Yousefi <elioryous@gmail.com>
2023-04-28 22:50:03 +0300

    main cpp

---
+++ Trivia-Project/CPP/Source.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <exception>
+#include <thread>
+#include "Server.h"
+#include <map>
+#include <string>
+
+#define PORT 8200
+#define EXIT "EXIT"
+
+int main()
+{
+	Server myServer;
+	std::string input;
+
+	try
+	{
+		std::thread t_connector(&Server::run, std::ref(myServer));
+
+		t_connector.detach();
+	}
+	catch (std::exception& e)
+	{
+		std::cout << "Error occured: " << e.what() << std::endl;
+	}
+
+	while (std::getline(std::cin, input) && input != EXIT);
+
+	// release resources
+
+	return 0;
+}

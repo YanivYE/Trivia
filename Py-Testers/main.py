cdbe36f07e6d13ac7f84bee0691d45b309b1087e
YanivYE <yanivye106@gmail.com>
2023-04-29 00:15:22 +0300

    finished python script version 1.0.1

--- Py-Testers/main.py
+++ Py-Testers/main.py
@@ -1,10 +1,13 @@
 import socket
 
+PORT = 8200
+MESSAGE = 'Hello'
+
 # create a socket object
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # get the server IP address and port number
-server_address = ('localhost', 8200)
+server_address = ('localhost', PORT)
 
 # connect to the server
 client_socket.connect(server_address)
@@ -13,10 +16,11 @@ client_socket.connect(server_address)
 response = client_socket.recv(1024).decode()
 print("Message received: " + response)
 
-# send a message to the server
-message = 'Hello'
-client_socket.send(message.encode())
-print("Sent message: " + message)
+if response == MESSAGE:
+    # send a message to the server
+    message = MESSAGE
+    client_socket.send(message.encode())
+    print("Sent message: " + message)
 
 # close the socket
 client_socket.close()

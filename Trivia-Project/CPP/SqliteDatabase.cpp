3dc0b8c0dd6f5aea993f33f808b8cec525601340
YanivYE <yanivye106@gmail.com>
2023-05-16 00:49:25 +0300

    added table creation and query functions

--- Trivia-Project/CPP/SqliteDatabase.cpp
+++ Trivia-Project/CPP/SqliteDatabase.cpp
@@ -12,7 +12,23 @@ bool SqliteDatabase::open()
 		std::cout << "Failed to open DB" << std::endl;
 		return false;
 	}
+	if (file_exist != 0)
+	{
+		// create DB table
+		createTable();
+	}
     return true;
+}
+
+void SqliteDatabase::createTable()
+{
+	// sql query
+	const char* usersTableQuery = "CREATE TABLE users (username TEXT NOT NULL, password TEXT NOT NULL, mail TEXT NOT NULL);";
+
+	char* errMessage = nullptr;
+	int res = sqlite3_exec(db, usersTableQuery, nullptr, nullptr, &errMessage);
+	if (res != SQLITE_OK)
+		std::cerr << errMessage;
 }
 
 bool SqliteDatabase::close()
@@ -21,9 +37,27 @@ bool SqliteDatabase::close()
     db = nullptr;
 }
 
+int userExistsCallback(void* existsPtr, int argc, char** argv, char** azColName)
+{
+	bool* exists = static_cast<bool*>(existsPtr);
+	*exists = (argc > 0);
+	return (*exists) ? 1 : 0;
+}
+
 int SqliteDatabase::doesUserExist(std::string name)
 {
-	return 0;
+	int exists = FALSE;
+	std::string query = "SELECT * FROM users WHERE username = '" + name + "';";
+
+	char* errMessage = nullptr;
+	int res = sqlite3_exec(db, query.c_str(), userExistsCallback, &exists, &errMessage);
+	if (res != SQLITE_OK)
+	{
+		std::cerr << errMessage;
+		return FALSE;
+	}
+		
+	return exists;
 }
 
 int SqliteDatabase::doesPasswordMatch(std::string originalPassword, std::string password)
@@ -33,5 +67,9 @@ int SqliteDatabase::doesPasswordMatch(std::string originalPassword, std::string 
 
 int SqliteDatabase::addNewUser(std::string name, std::string password, std::string mail)
 {
-	return 0;
+	char* errMessage = nullptr;
+	std::string query = "INSERT INTO users (username, password, mail) VALUES ('" + name + "','" + password + "','" + mail + "');";
+	int res = sqlite3_exec(db, query.c_str(), nullptr, nullptr, &errMessage);
+	if (res != SQLITE_OK)
+		std::cerr << errMessage;
 }
--- Trivia-Project/Headers/SqliteDatabase.h
+++ Trivia-Project/Headers/SqliteDatabase.h
@@ -2,11 +2,16 @@
 
 #include "IDataBase.h"
 
+enum boolVal{FALSE, TRUE};
+
 class SqliteDatabase : public IDataBase
 {
+public:
 	bool open() override;
 	bool close() override;
 	int doesUserExist(std::string name) override;
 	int doesPasswordMatch(std::string originalPassword, std::string password) override;
 	int addNewUser(std::string name, std::string password, std::string mail) override;
+private:
+	void createTable();
 };

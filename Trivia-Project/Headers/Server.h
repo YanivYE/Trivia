7bcf4586b4c126fb06b2d2b0130b0787b9b33d71
YanivYE <yanivye106@gmail.com>
2023-05-23 02:31:42 +0300

    started working on 1.0.4 bonus

--- Trivia-Project/Headers/Server.h
+++ Trivia-Project/Headers/Server.h
@@ -7,12 +7,27 @@
 class Server
 {
 public:
-	Server(); // constructor for server
-	~Server(); // desturctor for server
+	static Server& getInstance() {
+		static Server instance;
+		return instance;
+	}
 
 	void run(int port); // run server
 
 private:
+	Server() {
+		// Private constructor to prevent direct instantiation
+		m_database = new SqliteDatabase();
+		m_handlerFactory = new RequestHandlerFactory(this->m_database);
+		m_communicator = new Communicator(this->m_handlerFactory);
+	}
+
+	~Server() {
+		delete m_communicator;
+		delete m_database;
+		delete m_handlerFactory;
+	}
+
 	Communicator* m_communicator; // communicator object
 	IDataBase* m_database; // data base object
 	RequestHandlerFactory* m_handlerFactory; // request handler factory object

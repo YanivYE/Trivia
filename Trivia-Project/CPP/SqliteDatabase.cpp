76e19a1b92a078b404c27dd8e8d85825539218b6
YanivYE <yanivye106@gmail.com>
2023-05-22 18:55:36 +0300

    added comments

--- Trivia-Project/CPP/SqliteDatabase.cpp
+++ Trivia-Project/CPP/SqliteDatabase.cpp
@@ -1,9 +1,14 @@
 #include "../Headers/SqliteDatabase.h"
 
+// DB C'tor
 SqliteDatabase::SqliteDatabase()
 {
 }
 
+/*
+* Function opens a data base for the program
+* Output: data base opened or not
+*/
 bool SqliteDatabase::open()
 {
 	std::string dbFileName = DB_NAME;
@@ -24,16 +29,25 @@ bool SqliteDatabase::open()
     return true;
 }
 
+/*
+* Function opens a data base for the program
+* Output: data base opened or not
+*/
 void SqliteDatabase::createTable()
 {
 	const char* usersTableQuery = "CREATE TABLE users (username TEXT NOT NULL, password TEXT NOT NULL, mail TEXT NOT NULL);";
 
 	char* errMessage = nullptr;
+	// execute query
 	int res = sqlite3_exec(db, usersTableQuery, nullptr, nullptr, &errMessage);
 	if (res != SQLITE_OK)
 		std::cerr << errMessage;
 }
 
+/*
+* Function closes the data base 
+* Output: data base closed or not
+*/
 bool SqliteDatabase::close()
 {
     int res = sqlite3_close(db);
@@ -42,6 +56,14 @@ bool SqliteDatabase::close()
 	return res;
 }
 
+/*
+* Function gets if a user exists
+* Input: data - additional passed data, in this case, an album list
+* argc - number of collums
+* argv - array of strings that contains the values of the columns in the current row of the query result
+* azColName - array of strings that contains the names of the columns in the current row of the query result
+* Output: code 0
+*/
 int userExistsCallback(void* existsPtr, int argc, char** argv, char** azColName)
 {
 	int* exists = static_cast<int*>(existsPtr);
@@ -49,12 +71,19 @@ int userExistsCallback(void* existsPtr, int argc, char** argv, char** azColName)
 	return 0;
 }
 
+
+/*
+* Function checks if a user exists
+* Input: name - the user name
+* Output: user exists or not
+*/
 int SqliteDatabase::doesUserExist(std::string name)
 {
 	int exists = false;
 	std::string query = "SELECT * FROM users WHERE username = '" + name + "';";
 
 	char* errMessage = nullptr;
+	// execute query
 	int res = sqlite3_exec(db, query.c_str(), userExistsCallback, &exists, &errMessage);
 	if (res != SQLITE_OK)
 	{
@@ -65,6 +94,14 @@ int SqliteDatabase::doesUserExist(std::string name)
 	return exists;
 }
 
+/*
+* Function gets the user password
+* Input: data - additional passed data, in this case, an album list
+* argc - number of collums
+* argv - array of strings that contains the values of the columns in the current row of the query result
+* azColName - array of strings that contains the names of the columns in the current row of the query result
+* Output: code 0
+*/
 int userPasswordCallback(void* existsPtr, int argc, char** argv, char** azColName)
 {
 	std::string* password = static_cast<std::string*>(existsPtr);
@@ -72,20 +109,29 @@ int userPasswordCallback(void* existsPtr, int argc, char** argv, char** azColNam
 	return 0;
 }
 
+/*
+* Function checks if a user password is equivilant to a given password
+* Input: name - the user name
+* password - a given password
+* Output: password match or not
+*/
 int SqliteDatabase::doesPasswordMatch(std::string name, std::string password)
 {
 	if (doesUserExist(name))
+		// if the user exists
 	{
 		std::string userPassword = "";
 		std::string query = "SELECT password FROM users WHERE username = '" + name + "';";
 
 		char* errMessage = nullptr;
+		// execute query
 		int res = sqlite3_exec(db, query.c_str(), userPasswordCallback, &userPassword, &errMessage);
 		if (res != SQLITE_OK)
 		{
 			std::cerr << errMessage;
 			return false;
 		}
+		// return if the passwords match
 		return password == userPassword;
 	}
 	else
@@ -95,10 +141,18 @@ int SqliteDatabase::doesPasswordMatch(std::string name, std::string password)
 	}
 }
 
+/*
+* Function adds a new user
+* Input: name - the user name
+* password - a given password
+* mail - user namil
+* Output: user added or not
+*/
 int SqliteDatabase::addNewUser(std::string name, std::string password, std::string mail)
 {
 	char* errMessage = nullptr;
 	std::string query = "INSERT INTO users (username, password, mail) VALUES ('" + name + "','" + password + "','" + mail + "');";
+	// execute query
 	int res = sqlite3_exec(db, query.c_str(), nullptr, nullptr, &errMessage);
 	if (res != SQLITE_OK)
 	{

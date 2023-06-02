65d32ed5121e08cdab4c91d9f97da4d1d4fa202a
Elior Yousefi <elioryous@gmail.com>
2023-06-02 17:36:48 +0300

    added base function headers

--- Trivia-Project/Headers/IDataBase.h
+++ Trivia-Project/Headers/IDataBase.h
@@ -19,5 +19,13 @@ public:
 	virtual int doesPasswordMatch(std::string username, std::string password) = 0;
 	virtual int addNewUser(std::string username, std::string password, std::string mail) = 0;
 
+	// TODO: get questions
+	virtual float getPlayerAverageAnswerTime(std::string username) = 0;
+	virtual int getNumOfCorrectAnswers(std::string username) = 0;
+	virtual int getNumOfTotalAnswers(std::string username) = 0;
+	virtual int getNumOfPlayerGames(std::string username) = 0;
+	virtual int getPlayerScore(std::string username) = 0;
+	virtual std::vector<std::string> getHighScores() = 0;
+
 	sqlite3* db; // db
 };

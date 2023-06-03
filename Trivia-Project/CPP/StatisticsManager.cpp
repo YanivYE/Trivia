ebca392983210d2e4d41da28e77f43a37b53bb22
YanivYE <yanivye106@gmail.com>
2023-06-04 01:13:46 +0300

    finished 2.0.0 version

--- Trivia-Project/CPP/StatisticsManager.cpp
+++ Trivia-Project/CPP/StatisticsManager.cpp
@@ -7,12 +7,16 @@ StatisticsManager::StatisticsManager(IDataBase* db)
 
 std::vector<std::string> StatisticsManager::getHighScore()
 {
-	// TODO: AFTER MERGE
-	return std::vector<std::string>();
+	return m_database->getHighScores();
 }
 
 std::vector<std::string> StatisticsManager::getUserStatistics(std::string username)
 {
-	// TODO: AFTER MERGE
-	return std::vector<std::string>();
+	std::vector<std::string> stats;	
+	stats.push_back(std::to_string(m_database->getPlayerAverageAnswerTime(username)));
+	stats.push_back(std::to_string(m_database->getNumOfCorrectAnswers(username)));
+	stats.push_back(std::to_string(m_database->getNumOfTotalAnswers(username)));
+	stats.push_back(std::to_string(m_database->getNumOfPlayerGames(username)));
+	stats.push_back(std::to_string(m_database->getPlayerScore(username)));
+	return stats;
 }

7cbb66598ff92d2e3186e7a280506b1366734275
Elior Yousefi <elioryous@gmail.com>
2023-06-05 18:46:37 +0300

    added base gui

---
+++ TriviaGUI/Form1.cs
@@ -0,0 +1,15 @@
+namespace TriviaGUI
+{
+    public partial class mainForm : Form
+    {
+        public mainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
